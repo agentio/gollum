d64b4cf399bea735d82840dbfc4e99fd192aa1c7
Tim Burks <timburks@me.com>
2026-01-14 08:55:28 -0800

    do something for all property field types

--- pkg/lexica/generate.go
+++ pkg/lexica/generate.go
@@ -252,7 +252,7 @@ func renderproperties(lexicon *Lexicon, defname string, properties map[string]Pr
 				s += capitalize(propname) + " *string `json:" + `"` + propname + `,omitempty"` + "`\n"
 			}
 		case "array":
-			itemstype := resolveItemsType(lexicon, property.Items)
+			itemstype := resolveItemsType(lexicon, defname, propname, property.Items)
 			if required {
 				s += capitalize(propname) + " []" + itemstype + " `json:" + `"` + propname + `,omitempty"` + "`\n"
 			} else {
@@ -276,8 +276,20 @@ func renderproperties(lexicon *Lexicon, defname string, properties map[string]Pr
 		case "union":
 			uniontype := resolveUnionType(lexicon, defname, propname)
 			s += capitalize(propname) + " " + uniontype + " `json:" + `"` + propname + `,omitempty"` + "`\n"
+		case "bytes":
+			if required {
+				s += capitalize(propname) + " []byte `json:" + `"` + propname + `"` + "`\n"
+			} else {
+				s += capitalize(propname) + " *[]byte `json:" + `"` + propname + `,omitempty"` + "`\n"
+			}
+		case "cid-link":
+			if required {
+				s += capitalize(propname) + " string `json:" + `"` + propname + `,omitempty"` + "`\n"
+			} else {
+				s += capitalize(propname) + " *string `json:" + `"` + propname + `,omitempty"` + "`\n"
+			}
 		default:
-			s += "// FIXME: " + propname + " " + fmt.Sprintf("required=%t %+v", required, property) + "\n"
+			s += "// FIXME: unsupported property type " + propname + " " + property.Type + " " + fmt.Sprintf("required=%t %+v", required, property) + "\n"
 		}
 	}
 	return s
@@ -287,7 +299,7 @@ func renderunions(lexicon *Lexicon, defname string, properties map[string]Proper
 	var s string
 
 	var propnames []string
-	for propname, _ := range properties {
+	for propname := range properties {
 		propnames = append(propnames, propname)
 	}
 	sort.Strings(propnames)
@@ -298,22 +310,132 @@ func renderunions(lexicon *Lexicon, defname string, properties map[string]Proper
 			uniontype := resolveUnionType(lexicon, defname, propname)
 			s += "type " + uniontype + " struct {\n"
 			for _, ref := range property.Refs {
-				s += "// " + ref + "\n"
+				fieldname := unionfieldname(lexicon, ref)
+				fieldtype := unionfieldtype(lexicon, ref)
+				s += fieldname + " " + fieldtype + "\n"
 			}
-
 			s += "}\n\n"
+		case "array":
+			if property.Items.Type == "union" {
+				uniontype := resolveUnionType(lexicon, defname, propname) + "_Elem"
+				s += "type " + uniontype + " struct {\n"
+				for _, ref := range property.Items.Refs {
+					fieldname := unionfieldname(lexicon, ref)
+					fieldtype := unionfieldtype(lexicon, ref)
+					s += fieldname + " " + fieldtype + "\n"
+				}
+				s += "}\n\n"
+			}
 		}
 	}
 	return s
+}
+
+func unionfieldname(lexicon *Lexicon, ref string) string {
+	parts := strings.Split(ref, "#")
+	if len(parts) == 2 || len(parts) == 1 {
+		var id string
+		var tag string
+		if len(parts) == 2 {
+			id = parts[0]
+			tag = parts[1]
+		} else {
+			id = parts[0]
+			tag = "main"
+		}
+		if id == "" {
+			id = lexicon.Id
+		}
+
+		var reftype string
+		reflexicon := Lookup(id)
+		if reflexicon != nil {
+			refdef := reflexicon.Lookup(tag)
+			if refdef != nil {
+				reftype = refdef.Type
+			}
+		}
+
+		idparts := strings.Split(id, ".")
+		if len(idparts) != 4 {
+			return "/* FIXME " + fmt.Sprintf("%+v", ref) + " */ string"
+		}
+		name := capitalize(idparts[2]) + capitalize(idparts[3])
+		if tag != "main" {
+			name += "_" + capitalize(tag)
+		}
+
+		if reftype == "array" {
+			return "[]" + name + "_Elem"
+		}
+
+		return name
+	} else {
+		return "/* FIXME union field ref " + fmt.Sprintf("%+v", ref) + " */ string"
+	}
+}
+
+func unionfieldtype(lexicon *Lexicon, ref string) string {
+	parts := strings.Split(ref, "#")
+	if len(parts) == 2 || len(parts) == 1 {
+		var id string
+		var tag string
+		if len(parts) == 2 {
+			id = parts[0]
+			tag = parts[1]
+		} else {
+			id = parts[0]
+			tag = "main"
+		}
+		if id == "" {
+			id = lexicon.Id
+		}
+		var reftype string
+		reflexicon := Lookup(id)
+		if reflexicon != nil {
+			refdef := reflexicon.Lookup(tag)
+			if refdef != nil {
+				reftype = refdef.Type
+			}
+		}
+
+		idparts := strings.Split(id, ".")
+		if len(idparts) != 4 {
+			return "/* FIXME " + fmt.Sprintf("%+v", ref) + " */ string"
+		}
+		name := capitalize(idparts[2]) + capitalize(idparts[3])
+		if tag != "main" {
+			name += "_" + capitalize(tag)
+		}
+		// is the ref target in the same package as the lexicon?
+		// if not, we need to add the package name prefix
+
+		if !strings.HasPrefix(lexicon.Id, idparts[0]+"."+idparts[1]+".") {
+			prefix := idparts[0] + "_" + idparts[1]
+			name = prefix + "." + name
+		}
+
+		if reftype == "array" {
+			return "[]" + name + "_Elem"
+		}
+
+		return "*" + name
+	} else {
+		return "/* FIXME union field ref " + fmt.Sprintf("%+v", ref) + " */ string"
+	}
 }
 
 func resolveUnionType(lexicon *Lexicon, defname, propname string) string {
 	return capitalize(defname) + "_" + capitalize(propname) // "string"
 }
 
-func resolveItemsType(lexicon *Lexicon, items *Items) string {
+func resolveItemsType(lexicon *Lexicon, defname, propname string, items *Items) string {
 	switch items.Type {
 	case "string":
+		return "string"
+	case "unknown":
+		return "interface{}"
+	case "cid-link":
 		return "string"
 	case "ref":
 		ref := items.Ref
@@ -326,11 +448,16 @@ func resolveItemsType(lexicon *Lexicon, items *Items) string {
 			return "*" + typename
 		} else {
 			parts := strings.Split(ref, "#")
-			if len(parts) != 2 {
-				return "/* FIXME " + fmt.Sprintf("%+v", ref) + " */ string"
+			if len(parts) != 2 && len(parts) != 1 {
+				return "/* FIXME not enough parts" + fmt.Sprintf("%+v", ref) + " */ string"
 			}
 			id := parts[0]
-			tag := parts[1]
+			var tag string
+			if len(parts) == 2 {
+				tag = parts[1]
+			} else {
+				tag = "main"
+			}
 			idparts := strings.Split(id, ".")
 			if len(idparts) != 4 {
 				return "/* FIXME " + fmt.Sprintf("%+v", ref) + " */ string"
@@ -349,9 +476,11 @@ func resolveItemsType(lexicon *Lexicon, items *Items) string {
 
 			return "*" + name
 		}
+	case "union":
+		return "*" + capitalize(defname) + "_" + capitalize(propname+"_Elem")
 	default:
 	}
-	return "/* FIXME */ string"
+	return "/* FIXME unsupported items type: " + items.Type + " */ string"
 }
 
 func resolveRefType(lexicon *Lexicon, ref string) string {
