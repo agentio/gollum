ce5dcec46a4dd60fa7b4b725bcc24ed9f611910d
Tim Burks <timburks@me.com>
2026-01-16 10:42:47 -0800

    refine warnings, replace "FIXME" with "CHECKME" for lesser problems

--- pkg/lexica/generate-def.go
+++ pkg/lexica/generate-def.go
@@ -31,9 +31,9 @@ func (lexicon *Lexicon) generateDef(s *strings.Builder, def *Def, name string, p
 		s.WriteString("// " + def.Description + "\n")
 		s.WriteString("const " + defname + " string = " + `"` + name + `"` + "\n\n")
 	case "permission-set":
-		s.WriteString("// FIXME skipping permission set " + defname + "\n")
+		s.WriteString("// CHECKME skipping permission set " + defname + "\n")
 	case "subscription":
-		s.WriteString("// FIXME skipping subscription " + defname + "\n")
+		s.WriteString("// CHECKME skipping subscription " + defname + "\n")
 	default:
 		log.Warnf("skipping %s.%s (type %s)", lexicon.Id, name, def.Type)
 	}
