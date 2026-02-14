c3dcf035055cef712860a07923f9151378268d90
Tim Burks <timburks@me.com>
2026-02-14 11:00:56 -0800

    stop sending x-bsky-topics test header

--- pkg/froda/client.go
+++ pkg/froda/client.go
@@ -143,8 +143,6 @@ func (c *Client) Do(
 		log.Infof("proxy-session: %s", proxysession)
 	}
 
-	req.Header.Set("x-bsky-topics", strings.Repeat("this is my big header, yes..... ", 1))
-
 	userdid := c.UserDid
 	if userdid == "" {
 		userdid = os.Getenv("SLINK_USERDID")
